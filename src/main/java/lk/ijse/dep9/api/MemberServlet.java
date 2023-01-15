fd6c3aee4af3699cc81ede985e2054c431a92287
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-15 18:31:26 +0530

    Complete MemberServlet doGet method logic

--- src/main/java/lk/ijse/dep9/api/MemberServlet.java
+++ src/main/java/lk/ijse/dep9/api/MemberServlet.java
@@ -7,11 +7,69 @@ import lk.ijse.dep9.api.util.NewHttpServlet;
 import lk.ijse.dep9.exception.ResponseStatusException;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "member-servlet", value = "/members/*")
 public class MemberServlet extends NewHttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getPathInfo() == null || request.getPathInfo().equals("/")) {
+            String query = request.getParameter("q");
+            String size = request.getParameter("size");
+            String page = request.getParameter("page");
+            if (query != null && size != null && page != null) {
+                if (!size.matches("\\d+") || !page.matches("\\d++")) {
+                    throw new ResponseStatusException(400, "Invalid page or size");
+                }
+                else {
+                    searchMembersByPage(query, Integer.parseInt(size), Integer.parseInt(page), response);
+                }
+            }
+            else if (query != null && size == null && page == null) {
+                searchMembers(query, response);
+            }
+            else if (query == null && size != null && page != null) {
+                if (!size.matches("\\d+") || !page.matches("\\d+")) {
+                    throw new ResponseStatusException(400, "Invalid page or size");
+                } else {
+                    loadMembersByPage(Integer.parseInt(size), Integer.parseInt(page), response);
+                }
+            }
+            else {
+                loadAllMembers(response);
+            }
+        }
+        else {
+            String pathInfo = request.getPathInfo();
+            Matcher matcher = Pattern.compile("^/([A-Fa-f\\d]{8}(-[A-Fa-f\\d]{4}){3}-[A-Fa-f\\d]{12})/?$").matcher(pathInfo);
+            if (matcher.matches()) {
+                String uUID = matcher.group(1);
+                getMemberDetails(uUID, response);
+            }
+            else {
+                throw new ResponseStatusException(501);
+            }
+        }
+    }
+
+    private void loadAllMembers(HttpServletResponse response) {
+
+    }
+
+    private void loadMembersByPage(int size, int page, HttpServletResponse response) {
+
+    }
+
+    private void searchMembers(String query, HttpServletResponse response) {
+
+    }
+
+    private void searchMembersByPage(String query, int size, int page, HttpServletResponse response) {
+
+    }
+
+    private void getMemberDetails(String uUID, HttpServletResponse response) {
 
     }
 
