206011ed9217ffcea68e30a535dcb615d0f06c36
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-15 18:15:04 +0530

    Add Exception filter

--- src/main/java/lk/ijse/dep9/api/MemberServlet.java
+++ src/main/java/lk/ijse/dep9/api/MemberServlet.java
@@ -4,6 +4,7 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import lk.ijse.dep9.api.util.NewHttpServlet;
+import lk.ijse.dep9.exception.ResponseStatusException;
 
 import java.io.IOException;
 
@@ -11,7 +12,7 @@ import java.io.IOException;
 public class MemberServlet extends NewHttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("Hello");
+
     }
 
     @Override
