77d8dc403161120777d98e4e2b1d40160f9c015b
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-16 09:08:16 +0530

    Create IssueNoteDTO class

---
+++ src/main/java/lk/ijse/dep9/dto/IssueNoteDTO.java
@@ -0,0 +1,20 @@
+package lk.ijse.dep9.dto;
+
+import jakarta.json.bind.annotation.JsonbPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonbPropertyOrder({"id", "date", "memberId", "books"})
+public class IssueNoteDTO {
+    private Integer id;
+    private LocalDate date;
+    private String memberId;
+    private ArrayList<String> books = new ArrayList<>();
+}
