69218a2b7765aa9bf0b727e5e2a33364f75b686a
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-16 09:58:54 +0530

    Complete ReturnServlet doPost method logic

--- src/main/java/lk/ijse/dep9/api/ReturnServlet.java
+++ src/main/java/lk/ijse/dep9/api/ReturnServlet.java
@@ -1,10 +1,15 @@
 package lk.ijse.dep9.api;
 
 import jakarta.annotation.Resource;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.json.bind.JsonbException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import lk.ijse.dep9.api.util.NewHttpServlet;
+import lk.ijse.dep9.dto.IssueNoteDTO;
+import lk.ijse.dep9.dto.ReturnDTO;
+import lk.ijse.dep9.exception.ResponseStatusException;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -17,6 +22,26 @@ public class ReturnServlet extends NewHttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getPathInfo() == null || request.getPathInfo().equals("/")) {
+            if (request.getContentType() == null || !request.getContentType().startsWith("application/json")) {
+                throw new ResponseStatusException(400, "Invalid Content Type");
+            }
+            else {
+                try {
+                    ReturnDTO returnDTO = JsonbBuilder.create().fromJson(request.getReader(), ReturnDTO.class);
+                    addReturnItems(returnDTO, response);
+                }
+                catch (JsonbException e) {
+                    throw new ResponseStatusException(400, "Return Note JSON format is incorrect");
+                }
+            }
+        }
+        else {
+            throw new ResponseStatusException(501);
+        }
+    }
+
+    private void addReturnItems(ReturnDTO returnDTO, HttpServletResponse response) {
 
     }
 }
