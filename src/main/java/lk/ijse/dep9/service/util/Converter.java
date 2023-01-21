7eb0321a4c0748d4313a31b6b5b5e20ff64a7a8f
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-21 11:50:11 +0530

    Implement DTO-Entity mapping of Book and Member

--- src/main/java/lk/ijse/dep9/service/util/Converter.java
+++ src/main/java/lk/ijse/dep9/service/util/Converter.java
@@ -1,4 +1,32 @@
 package lk.ijse.dep9.service.util;
 
+import lk.ijse.dep9.dto.BookDTO;
+import lk.ijse.dep9.dto.MemberDTO;
+import lk.ijse.dep9.entity.Book;
+import lk.ijse.dep9.entity.Member;
+import org.modelmapper.ModelMapper;
+
 public class Converter {
+
+    private ModelMapper modelMapper;
+
+    public Converter(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    public BookDTO toBookDTO(Book book) {
+        return modelMapper.map(book, BookDTO.class);
+    }
+
+    public Book toBookEntity(BookDTO bookDTO) {
+        return modelMapper.map(bookDTO, Book.class);
+    }
+
+    public MemberDTO toMemberDTO(Member member) {
+        return modelMapper.map(member, MemberDTO.class);
+    }
+
+    public Member toMemberEntity(MemberDTO memberDTO) {
+        return modelMapper.map(memberDTO, Member.class);
+    }
 }
---
+++ src/test/java/lk/ijse/dep9/service/util/ConverterTest.java
@@ -0,0 +1,23 @@
+package lk.ijse.dep9.service.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ConverterTest {
+
+    @Test
+    void toBookDTO() {
+    }
+
+    @Test
+    void toBookEntity() {
+    }
+
+    @Test
+    void toMemberDTO() {
+    }
+
+    @Test
+    void toMemberEntity() {
+    }
+}
