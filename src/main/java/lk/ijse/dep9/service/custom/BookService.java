fcb827cd76fa8daafb35981e6883fdb2683ce46e
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-21 14:09:14 +0530

    Make agreements in BookService interface

--- src/main/java/lk/ijse/dep9/service/custom/BookService.java
+++ src/main/java/lk/ijse/dep9/service/custom/BookService.java
@@ -1,6 +1,25 @@
 package lk.ijse.dep9.service.custom;
 
+import lk.ijse.dep9.dto.BookDTO;
 import lk.ijse.dep9.service.SuperService;
+import lk.ijse.dep9.service.exception.DuplicateException;
+import lk.ijse.dep9.service.exception.NotFoundException;
+
+import java.util.List;
 
 public interface BookService extends SuperService {
+
+    List<BookDTO> getAllBooks();
+
+    List<BookDTO> getBooksByPage(int size, int page);
+
+    List<BookDTO> findBooks(String query);
+
+    List<BookDTO> findBooksByPage(String query, int size, int page);
+
+    BookDTO getBookInfo(String isbn) throws NotFoundException;
+
+    BookDTO addNewBook(BookDTO bookDTO) throws DuplicateException;
+
+    BookDTO updateBookDetails(BookDTO bookDTO) throws NotFoundException;
 }
