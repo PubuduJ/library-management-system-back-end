4cd48040fb98d674c421e3e0fee8c53b614a3748
Pubudu Janith <pubudujanith123@gmail.com>
2023-01-21 20:45:04 +0530

    Retest MemberServiceImpl

--- src/test/java/lk/ijse/dep9/service/custom/impl/MemberServiceImplTest.java
+++ src/test/java/lk/ijse/dep9/service/custom/impl/MemberServiceImplTest.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep9.service.custom.impl;
 
 import lk.ijse.dep9.dto.MemberDTO;
+import lk.ijse.dep9.service.exception.InUseException;
 import lk.ijse.dep9.service.exception.NotFoundException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -52,20 +53,20 @@ class MemberServiceImplTest {
 
     @Test
     void findMembers() {
-        List<MemberDTO> members = memberServiceImpl.findMembers("Tharindu");
+        List<MemberDTO> members = memberServiceImpl.findMembers("Pubudu");
         assertEquals(1, members.size());
     }
 
     @Test
     void findMembersByPage() {
-        List<MemberDTO> members = memberServiceImpl.findMembersByPage("Tharindu", 1, 1);
+        List<MemberDTO> members = memberServiceImpl.findMembersByPage("Pubudu", 1, 1);
         assertEquals(1, members.size());
     }
 
     @Test
     void getMemberInfo() {
-        MemberDTO memberInfo = memberServiceImpl.getMemberInfo("2714641a-301e-43d5-9d31-ad916d075ba6");
-        assertEquals("2714641a-301e-43d5-9d31-ad916d075ba6", memberInfo.getId());
+        MemberDTO memberInfo = memberServiceImpl.getMemberInfo("104ccff3-c584-4782-a582-8a06479b4600");
+        assertEquals("104ccff3-c584-4782-a582-8a06479b4600", memberInfo.getId());
     }
 
     @Test
@@ -79,15 +80,15 @@ class MemberServiceImplTest {
 
     @Test
     void deleteMember() {
-        memberServiceImpl.deleteMember("2714641a-301e-43d5-9d31-ad916d075ba6");
-        assertThrows(NotFoundException.class, () -> {
-            memberServiceImpl.deleteMember("2714641a-301e-43d5-5632-ad916d075ba6");
+        memberServiceImpl.deleteMember("2714641a-301e-43d5-9d31-ad916d075800");
+        assertThrows(InUseException.class, () -> {
+            memberServiceImpl.deleteMember("104ccff3-c584-4782-a582-8a06479b4600");
         });
     }
 
     @Test
     void updateMemberDetails() {
-        MemberDTO memberDTO = new MemberDTO("2714641a-301e-43d5-9d31-ad916d075ba6", "Kasun", "Kaluthara", "071-5623698");
+        MemberDTO memberDTO = new MemberDTO("2714641a-301e-43d5-9d31-ad916d075700", "Lilan Sachintha", "Rathmalana", "071-5623698");
         MemberDTO updatedMember = memberServiceImpl.updateMemberDetails(memberDTO);
         assertEquals(memberDTO.getId(), updatedMember.getId());
         assertEquals(memberDTO.getName(), updatedMember.getName());
